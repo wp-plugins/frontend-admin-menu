ba8dcbc8d3274fbd2ff962639f3ae8aabbcd9dad
studiosweb <studiosweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-21 09:09:26 +0000

    New version 1.7
    git-svn-id: https://plugins.svn.wordpress.org/frontend-admin-menu/tags/1.7@1164798 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/options_admin_page.php
@@ -0,0 +1,10 @@
+<div class="wrap">
+    <div id="frontend-admin-menu-backend">
+        <?php _e( '<h2>Frontend admin menu - Options</h2>', 'frontend-admin-menu' ); ?>
+        <div id="gs-middle">
+            <?php require_once( FRONTEND_ADMIN_MENU_DIR . "admin/includes/gs-middle-options-page.php" ); ?>
+        </div>
+    </div>
+</div>
+
+
