f1e8e1623dda8d51a9cc9e824a2505ed01a6c725
studiosweb <studiosweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-28 08:22:45 +0000

    Version 1.9
    git-svn-id: https://plugins.svn.wordpress.org/frontend-admin-menu/trunk@1169178 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/frontend-admin-menu.js
+++ js/frontend-admin-menu.js
@@ -1,12 +1,30 @@
+function frontend_admin_menu_setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function frontend_admin_menu_getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
 jQuery( "#frontend-admin-menu-icon" ).click(function() {
     jQuery( "#frontend-admin-menu-items" ).toggle();
-    var cexpand = jQuery.cookie('frontend-admin-menu');
+    var cexpand = frontend_admin_menu_getCookie('frontend-admin-menu');
     if (cexpand === undefined || null){
-        jQuery.cookie('frontend-admin-menu', 'expand-on', jQuery.extend({}, { path: '/' }, { expires: 91 }));
+        frontend_admin_menu_setCookie("frontend-admin-menu", 'expand-on', 365);
     } else if (cexpand === 'expand-on'){
-        jQuery.cookie('frontend-admin-menu', 'expand-off', jQuery.extend({}, { path: '/' }, { expires: 91 }));
+        frontend_admin_menu_setCookie("frontend-admin-menu", 'expand-off', 365);
     } else if (cexpand === 'expand-off'){
-        jQuery.cookie('frontend-admin-menu', 'expand-on', jQuery.extend({}, { path: '/' }, { expires: 91 }));
+        frontend_admin_menu_setCookie("frontend-admin-menu", 'expand-on', 365);
     }
 });
 
@@ -14,7 +32,7 @@ jQuery( document ).ready( function() {
     jQuery( '#frontend-admin-menu-items .menu-item-has-children > .expand' ).addClass( "frontend-admin-menu-childen-icon-right" );
     jQuery( '#frontend-admin-menu-menu #frontend-admin-menu-items' ).css( {display:'none'} );
     jQuery( '#frontend-admin-menu-items ul ul' ).hide();
-    var cexpand = jQuery.cookie('frontend-admin-menu');
+    var cexpand = frontend_admin_menu_getCookie('frontend-admin-menu');
     if (cexpand === undefined || null){
         jQuery( '#frontend-admin-menu-menu #frontend-admin-menu-items' ).css( {display:'none'} );
     } else if (cexpand === 'expand-on'){
